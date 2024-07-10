5ef69447adb3e6f6289e4e1e1e548e4185a713a7
Juan José García <juanjgar998@gmail.com>
2024-07-10 16:53:27 -0300

    Cambiar origin de CORS.

--- src/constant/cors.ts
+++ src/constant/cors.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface"
 
 export const cors: CorsOptions = {
-    origin: true,
+    origin: ['http://localhost:3000'],
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true,
 }
